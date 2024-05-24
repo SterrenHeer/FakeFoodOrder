b8c0da948b1ed48487e2324f67c89b0f29751a4e
SterrenHeer <sterrenheer@gmail.com>
2024-05-24 16:21:10 +0300

    realize post removing feature

--- src/App.js
+++ src/App.js
@@ -15,10 +15,17 @@ function App() {
 		setPosts([...posts, newPost])
 	} 
 
+	const removePost = (post) => {
+		setPosts(posts.filter(p => p.id !== post.id))
+	} 
+
   	return (
 		<div className="App">
 			<PostForm create={createPost} />
-			<PostList posts={posts} title={'Список постов 1'} />
+			{posts.length
+				? <PostList remove={removePost} posts={posts} title={'Список постов 1'} />
+				: <h1 style={{textAlign: 'center'}}>Посты не найдены!</h1>
+			}
 		</div>
   	);
 }
--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -8,7 +8,7 @@ const PostItem = (props) => {
                 <div>{props.post.body}</div>
             </div>
             <div className="post_btns">
-                <button>Удалить</button>
+                <button onClick={() => props.remove(props.post)}>Удалить</button>
             </div>
         </div>
     );
