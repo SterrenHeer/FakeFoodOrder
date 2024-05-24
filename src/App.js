94001a9a90652c23b6274cc9be37b31e973c6ccb
SterrenHeer <sterrenheer@gmail.com>
2024-05-24 14:10:32 +0300

    add test post item component and some styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
+import PostItem from "./components/PostItem";
+
+import "./styles/App.scss"
 
 function App() {
   	return (
 		<div className="App">
 			<Counter />
 			<ClassCounter />
+			<PostItem />
 		</div>
   	);
 }
